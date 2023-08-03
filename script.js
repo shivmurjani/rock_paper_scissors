7c04ea0de2ad74e419515e5eaf0196c866c36d00
Shivansh Murjani <murjani.vmware@gmail.com>
2023-08-03 11:13:55 +0530

    code breaks when invalid input is provided

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 function getuserChoice(){
     let choice = prompt("Enter your move. ROCK/PAPER/SCISSORS").toLowerCase();
-    while(x!== "rock"&&x!=="paper"&&x!=="scissor"){
+    while(choice!== "rock"&&choice!=="paper"&&choice!=="scissor"){
         alert("invalid input, try again");
         x = getuserChoice();
     }
@@ -13,3 +13,29 @@ function getcomputerChoice(){
     botchoice = choices[Math.floor(Math.random()*3)];
     return botchoice;
 }
+
+function compareChoice(choice_a,choice_b){
+    //draw condition 
+    if(choice_a == choice_b){
+        return ["ITS A DRAW-","YOUR CHOICE - "+choice_a,"CPU CHOICE - "+choice_b];
+    }
+    else if (choice_a == "rock" && choice_b == "paper"){
+        return ["CPU WINS!!!","YOUR CHOICE - "+choice_a,"CPU CHOICE - "+choice_b];
+    }
+    else if (choice_a == "paper" && choice_b == "scissor"){
+        return ["CPU WINS!!!","YOUR CHOICE - "+choice_a,"CPU CHOICE - "+choice_b];
+    }
+    else if (choice_a == "scissor" && choice_b == "rock"){
+        return ["CPU WINS!!!","YOUR CHOICE - "+choice_a,"CPU CHOICE - "+choice_b];
+    }
+    else{
+        return ["YOU WIN!!!","YOUR CHOICE - "+choice_a,"CPU CHOICE - "+choice_b];;
+    }
+}
+
+for(let i=0;i<5;i++){
+    let x = getuserChoice();
+    let y = getcomputerChoice();
+    alert(compareChoice(x,y));
+}
+
