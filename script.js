a0aa5b7feec88369c06c684d6c16a4610b780edd
Shivansh Murjani <murjani.vmware@gmail.com>
2023-08-03 13:47:32 +0530

    created the game logic

--- script.js
+++ script.js
@@ -3,39 +3,55 @@ function getuserChoice(){
     let choice = prompt("Enter your move. ROCK/PAPER/SCISSORS").toLowerCase();
     while(choice!== "rock"&&choice!=="paper"&&choice!=="scissor"){
         alert("invalid input, try again");
-        return getuserChoice();
+        return getuserChoice(); //recursively calls until input is valid
     }
-    return choice;
+    return choice;//returns valid input
 }
 
 function getcomputerChoice(){
     const choices = ["rock","paper","scissor"];
     let botchoice = choices[Math.floor(Math.random()*3)];
-    return botchoice;
+    return botchoice; //random choice is returned
 }
 
-function compareChoice(choice_a,choice_b){
-    //draw condition 
-    if(choice_a == choice_b){
-        return ["ITS A DRAW-","YOUR CHOICE - "+choice_a,"CPU CHOICE - "+choice_b];
+//deciding winner
+// function compareChoice(choice_a,choice_b){
+//     //draw condition 
+//     if(choice_a == choice_b){
+//         return ["ITS A DRAW-","YOUR CHOICE - "+choice_a,"CPU CHOICE - "+choice_b];
+//     }
+//     else if (choice_a == "rock" && choice_b == "paper"){
+//         return ["CPU WINS!!!","YOUR CHOICE - "+choice_a,"CPU CHOICE - "+choice_b];
+//     }
+//     else if (choice_a == "paper" && choice_b == "scissor"){
+//         return ["CPU WINS!!!","YOUR CHOICE - "+choice_a,"CPU CHOICE - "+choice_b];
+//     }
+//     else if (choice_a == "scissor" && choice_b == "rock"){
+//         return ["CPU WINS!!!","YOUR CHOICE - "+choice_a,"CPU CHOICE - "+choice_b];
+//     }
+//     else{
+//         return ["YOU WIN!!!","YOUR CHOICE - "+choice_a,"CPU CHOICE - "+choice_b];;
+//     }
+// }
+
+
+//deciding winner USING A matrix!
+function giveResult(choice_a,choice_b){
+    const choices = {rock:0,paper:1,scissor:2};
+    const resultMatrix= [[0,-1,1],[1,0,-1],[-1,1,0]]; //9 possible outcomes
+    const userChoice = choices[choice_a];
+    const cpuChoice = choices[choice_b];
+
+    //CONDITIONS TO DECIDE----- VICTORY == 1, LOSS == -1, TIE == 0
+    if(resultMatrix[userChoice][cpuChoice]==0){
+        return "ITS A TIE";
     }
-    else if (choice_a == "rock" && choice_b == "paper"){
-        return ["CPU WINS!!!","YOUR CHOICE - "+choice_a,"CPU CHOICE - "+choice_b];
-    }
-    else if (choice_a == "paper" && choice_b == "scissor"){
-        return ["CPU WINS!!!","YOUR CHOICE - "+choice_a,"CPU CHOICE - "+choice_b];
-    }
-    else if (choice_a == "scissor" && choice_b == "rock"){
-        return ["CPU WINS!!!","YOUR CHOICE - "+choice_a,"CPU CHOICE - "+choice_b];
+    else if(resultMatrix[userChoice][cpuChoice]==1){
+        return "YOU WIN!!";
     }
     else{
-        return ["YOU WIN!!!","YOUR CHOICE - "+choice_a,"CPU CHOICE - "+choice_b];;
+        return "YOU LOSE :(";
     }
-}
 
-for(let i=0;i<5;i++){
-    let x = getuserChoice();
-    let y = getcomputerChoice();
-    alert(compareChoice(x,y));
 }
 
